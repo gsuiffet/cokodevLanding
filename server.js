98dcd47fc330baf2f07b2b883eada2ad7c18c56e
Guillaume Suiffet <suiffet.g@gmail.comm>
2017-10-09 23:33:54 +0200

    trello

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 var Trello = require("node-trello");
-var t = new Trello("674dce23795a54b9be3a090108a9ea45", "6ca9f47861320267f23f1406b36726fd4d6f76df584b946a5fa33a0ad1bbea89");
+var t = new Trello("YYYY-MM-DDTHH:MM:SSZ", "YYYY-MM-DDTHH:MM:SSZ");
 
 app.use(express.static('public'));
 
@@ -23,7 +23,7 @@ app.post('/contact', function (req, res) {
     console.log(req.body.message);
 // URL arguments are passed in as an object.
     //t.post("/1/members/me", { cards: "open" }, function(err, data) {
-    t.post("/1/cards", { idList: "59da6052f1efec82449f108a", name:req.body.name + " " +  req.body.email, desc: req.body.message}, function(err, data) {
+    t.post("/1/cards", { idList: "YYYY-MM-DDTHH:MM:SSZ", name:req.body.name + " " +  req.body.email, desc: req.body.message}, function(err, data) {
         if (err) throw err;
         console.log(data);
     });
