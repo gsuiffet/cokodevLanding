5aeb5b1488f608e95c903e7fbf0cbbb1d7779e25
Olivia Tastet <otastet@yahoo.com>
2017-11-17 16:38:37 +0100

    server port heroku 8080

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.post('/contact', function (req, res) {
     res.render('contact');
 });
 
-app.listen(8080, function () {
-    console.log("Server listening on port 8080");
+var port= (process.env.PORT || 8080);
+app.listen(port, function () {
+  console.log(“Server listening on port 8080”);
 });
